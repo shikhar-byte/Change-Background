40617c527fb70c3bcf46ce66bc899a5c49d29120
shikhar-byte <50840670+shikhar-byte@users.noreply.github.com>
2021-05-22 22:53:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const btn = document.querySelector('button');
+
+const body = document.querySelector('body');
+
+let colors =['red', 'blue', 'gray', 'green' , 'pink' , 'yellow' , 'seagreen' , 'orange' , 'purple'];
+
+btn.addEventListener('click', function(){
+
+    let colorIndex = Math.floor(Math.random() * colors.length );
+
+    body.style.backgroundColor = colors[colorIndex];
+})
